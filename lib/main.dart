78b5a2606df671a690af4b438d69e68202cbf26c
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2023-01-28 21:23:25 +0545

    add uxcam app key

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import "nav.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_uxcam/flutter_uxcam.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -17,8 +19,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    String appkey = dotenv.get("UXCAM_APP_KEY", fallback: "");
     FlutterUxcam.optIntoSchematicRecordings();
-    FlutterUxcam.startWithKey("UX_CAM_KEY");
+    FlutterUxcam.startWithKey(appkey);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
